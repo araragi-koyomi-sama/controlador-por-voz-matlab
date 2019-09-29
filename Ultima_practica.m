f0d1e3d97b535b49a5ffc9c88e9a6e4e7fdaa276
araragi-koyomi <alexander08063@gmail.com>
2019-09-29 13:23:59 -0500

    practica final

---
+++ Ultima_practica.m
@@ -0,0 +1,13 @@
+%Práctica 1 
+%coger 5  vocablos y generar una base de datos de los 5 vocablos en el excel con mínimo 7 tomas de cada uno de ellas...
+%limitados en el tiempo.
+%Cada una de ellas con un promedio en el analálisis espectral gráficado (transformada rápida de Fourier)
+%Un análisis de error entre ellas con su promedio
+
+%Práctica 2
+%Generar el movimiento y click del mouse (apuntador) posicionado en un
+%lugar de la pantalla, abrir imagenes, páginas web, entre otros 
+
+%Práctica 3
+%Hacer un sistema de reconocimiento de voz con todo lo anterior que me
+%identifique alguna característica
